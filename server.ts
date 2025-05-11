f62bfd0285889dfb49fe686f84a8af1026f81e5e
mstura21-FOI <mustra769@gmail.com>
2025-05-11 17:33:26 +0200

    Dodani admin kontrole i migracija na role based sa adminom i userom

--- server.ts
+++ server.ts
@@ -6,18 +6,18 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import { protect } from './middleware/defend';
-import router from './router';
+import {router, adminRouter} from './router';
 import { createNewUser, signin } from './handlers/user';
+import { protectAdmin } from "./middleware/defendAdmin";
 
 app.use(express.static("static"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', protect, router);
-
+app.use('/admin', protectAdmin, adminRouter);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
 app.post("/user", createNewUser);
 app.post("/signin", signin);
 
